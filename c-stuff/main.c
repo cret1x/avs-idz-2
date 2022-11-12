93cf2d2dfb508fe287402c096e622247a2188639
cret1x <33805149+cret1x@users.noreply.github.com>
2022-11-12 23:53:21 +0300

    Update main.c

--- c-stuff/main.c
+++ c-stuff/main.c
@@ -1,95 +1,68 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <time.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define ulli unsigned long long int
+#define ulli unsigned long long int 
 
-int main(int argc, char** argv) {
-	if (argc < 3) {
-		printf("Usage: ./main <input> <output>\n");
+
+char* read_string_from_file(FILE *f) {
+	char* ret = (char*) malloc(sizeof(char) * 134217728);
+	char ch;
+	ulli c = 0;
+	while (c < 134217728 && (ch = fgetc(f)) != EOF) {
+		if (ch > 127) {
+			return NULL;
+		}
+		ret[c++] = ch;
+	}
+	return ret;
+}
+
+int main(int argc, char const *argv[])
+{
+	if (argc < 2) {
+		printf("Invalid args count\n");
 		return 0;
 	}
-	FILE *fp;
-	ulli n;
-	int* arr;
-	int idx = 2;
-	clock_t start_read, end_read, start_calc, end_calc, start_write, end_write;
-	start_read = clock();
-	if (strcmp(argv[1], "-r")) {
-		fp = fopen(argv[1], "r");
-		if (fp == NULL) {
-			printf("Wrong fname\n");
+	if (strcmp(argv[1], "-f") == 0) {
+		if (argc < 5) {
+			printf("Invalid args count\n");
 			return 0;
 		}
-		fscanf(fp, "%llu", &n);
-		if (n > 100000000) {
-			printf("Length is greater than 100000000!\n");
-			fclose(fp);
+		FILE* string_file = fopen(argv[2], "r");
+		FILE* substring_file = fopen(argv[3], "r");
+		FILE* out_file = fopen(argv[4], "w");
+		if (string_file == NULL || substring_file == NULL || out_file == NULL) {
+			printf("Error opening the files\n");
 			return 0;
 		}
-		arr = (int*)malloc(sizeof(int)*n);
-		for (ulli i = 0; i < n; ++i) {
-			fscanf(fp, "%d", &arr[i]);
+		char* string = read_string_from_file(string_file);
+		char* substring = read_string_from_file(substring_file);
+		if (string == NULL || substring == NULL) {
+			printf("Invalid char over 127!!!\n");
+				free(string);
+				free(substring);
+				fclose(string_file);
+				fclose(substring_file);
+				return 0;
 		}
+		char *fnd = strstr(string, substring);
+		if (fnd) {
+			ulli position = fnd - string;
+			fprintf(out_file, "%llu\n", position);
+		} else {
+			fprintf(out_file, "-1\n");
+		}
+		free(string);
+		free(substring);
+		fclose(string_file);
+		fclose(substring_file);
+		fclose(out_file);
 
-		fclose(fp);
+	} else if (strcmp(argv[1], "-r") == 0) {
+		printf("Random mode\n");
 	} else {
-		n = atoll(argv[2]);
-		if (n > 100000000) {
-			printf("Length is greater than 100000000!\n");
-			return 0;
-		}
-		int lower_bound = atoi(argv[3]);
-		int upper_bound = atoi(argv[4]);
-		idx = 5;
-		arr = (int*)malloc(sizeof(int)*n);
-		srand(time(NULL));
-		for (ulli i = 0; i < n; ++i) {
-			arr[i] = (rand() % (upper_bound - lower_bound)) + lower_bound;
-			printf("%d ", arr[i]);
-		}
-		printf("\n");
+		printf("Invalid flag\n");
 	}
-	end_read = clock() - start_read;
-	
-	fp = fopen(argv[idx], "w");
-	if (fp == NULL) {
-		printf("Wrong fname\n");
-		free(arr);
-		return 0;
-	}
-	start_calc = clock();
-	int neg_idx = -1;
-	for (ulli i = 0; i < n; ++i) {
-		if (arr[i] < 0 && arr[i] % 2 == 0) {
-			neg_idx = i;
-			break;
-		}
-	}
-	if (neg_idx == -1) {
-		neg_idx = n - 1;
-	}
-	int* arrB = (int*)malloc(sizeof(int)*n);
-	for (ulli i = 0; i < n; ++i) {
-		arrB[i] = arr[i] * arr[neg_idx];
-	}
-	end_calc = clock() - start_calc;
-	start_write = clock();
-	for (ulli i = 0; i < n; ++i) {
-		fprintf(fp, "%d\n", arrB[i]);
-	}
-	end_write = clock() - start_write;
-	free(arr);
-	free(arrB);
-	fclose(fp);
-	double time_read = ((double)end_read)/CLOCKS_PER_SEC;
-	double time_calc = ((double)end_calc)/CLOCKS_PER_SEC;
-	double time_write = ((double)end_write)/CLOCKS_PER_SEC;
-	printf("Elapsed time:\n");
-	printf("Read:\t\t%f\n", time_read);
-	printf("Calculations:\t%f\n", time_calc);
-	printf("Write:\t\t%f\n", time_write);
 	return 0;
 }
